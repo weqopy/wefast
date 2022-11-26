35a07ac26403acdaf275ed766910ef29b86d4147
weqopy <weqopy@gmail.com>
2022-11-27 00:00:56 +0800

    schema, response_model

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from typing import Union
 import uvicorn
 from fastapi import FastAPI
 import sys
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 
 app = FastAPI()
@@ -14,16 +15,27 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "price": 35.4,
+                "is_offer": True,
+            }
+        }
+
 
 @app.get("/")
 async def read_root():
     await async_test()
-    return {"message": "Hello World"}
+    return {"message": f"{datetime.now()}"}
+
 
 async def async_test():
     print("async_test begin")
-    await asyncio.sleep(1)
+    # await asyncio.sleep(1)
     print("async_test end")
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -34,6 +46,24 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_id": item_id, **item.dict()}
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
 
 
 def main():
@@ -48,3 +78,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# nohup python3 main.py >  ./uvicorn.log 2>&1 &
