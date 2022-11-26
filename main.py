549cdcffbfffbc019d85f0f39b42dac23710d9ba
weqopy <weqopy@gmail.com>
2022-11-27 02:04:58 +0800

    optimize User* class

--- main.py
+++ main.py
@@ -48,22 +48,39 @@ def update_item(item_id: int, item: Item):
     return {"item_id": item_id, **item.dict()}
 
 
-class UserIn(BaseModel):
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: Union[str, None] = None
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Union[str, None] = None
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
 
 
 @app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
 
 
 def main():
