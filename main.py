f8be01a2a10a9b2d90669d4910a0c4c2b3f7dfbc
weqopy <weqopy@gmail.com>
2022-12-03 16:58:33 +0800

    up orm

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
-from typing import Union
 import uvicorn
-from fastapi import FastAPI, status, Form
-import sys
-from pydantic import BaseModel, EmailStr
+from fastapi import FastAPI, Depends, HTTPException
 from datetime import datetime
 
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -17,20 +19,65 @@ async def root():
 
 
 async def async_test():
-    asyncio.sleep(1)
+    await asyncio.sleep(1)
 
 
-def main():
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-        reload_delay=0.3,
-    )
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-if __name__ == "__main__":
-    main()
+# @app.post("/users/", response_model=schemas.User)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="Email already registered")
+#     return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
+# def create_item_for_user(
+#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+# ):
+#     return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+
+# def main():
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8000,
+#         reload=True,
+#         reload_delay=0.4,
+#     )
+
+
+# if __name__ == "__main__":
+#     main()
+
 
 # nohup python3 main.py >  ./uvicorn.log 2>&1 &
